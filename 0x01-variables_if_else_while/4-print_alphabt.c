7a1768a7471c91860c6462d80e950252729ab9d2
Thembi Adoorns <adoornsthembakazi@gmail.com>
2023-02-20 15:05:34 -0800

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and check its
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z'
+	{
+		if (c != 'e' && c != '4'
+		{
+			putchar(c);
+
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
